443afe4ad100e13666d6a200f4def793c654a7c2
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2022-10-17 15:43:49 +0200

    Bump upickle and os-lib

--- snunit-cli/config.scala
+++ snunit-cli/config.scala
@@ -1,3 +1,3 @@
 //> using scala "3"
-//> using lib "com.lihaoyi::upickle::1.5.0"
-//> using lib "com.lihaoyi::os-lib::0.8.0"
+//> using lib "com.lihaoyi::upickle::2.0.0"
+//> using lib "com.lihaoyi::os-lib::0.8.1"
